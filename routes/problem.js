73ea0f618fa1d012cbf10c9bbf6d3f0d6ed58eed
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-02-01 22:48:29 +0530

    ..

--- routes/problem.js
+++ routes/problem.js
@@ -3,7 +3,7 @@ var multer = require('multer');
 var router = express.Router();
 
 var problem_controller = require('../controllers/problem_controller');
-var upload = multer({dest: 'tmp/'});
+var upload = multer({ dest: 'tmp/' });
 
 router.get('/', problem_controller.get_problem_list);
 
@@ -13,13 +13,13 @@ router.get('/diff/:diff', problem_controller.get_all_problem_with_diff);
 
 router.get('/:pid', problem_controller.get_problem);
 
-router.post('/:pid',upload.single('submit-file'), problem_controller.post_submission);
+router.post('/:pid', upload.single('submit-file'), problem_controller.post_submission);
 
 router.post('/:pid/live', problem_controller.post_submission_live_editor);
 
 router.get('/:pid/clearcookie', function(req, res) {
     res.clearCookie("submitLang")
-    .redirect('/problems/' + req.params.pid);
+        .redirect('/problems/' + req.params.pid);
 });
 
 module.exports = router;
